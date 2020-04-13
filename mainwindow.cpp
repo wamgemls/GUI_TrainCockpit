cee47c64c70d5be06173c2e1fc28126228245019
wamgemls <wamgemls@gmail.com>
2020-04-13 16:47:40 +0200

    x

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -41,26 +41,6 @@ MainWindow::MainWindow(QWidget *parent)
 
     //getDataButton = new QPushButton("GetData", this);
 
-
-    startWorkerButton = new QPushButton("Start Worker Thread", this);
-    startWorkerButton->setGeometry(400, 100, 200, 50);
-
-    stopWorkerButton = new QPushButton("Stop Worker Thread", this);
-    stopWorkerButton->setGeometry(400, 200, 200, 50);
-
-    quitButton = new QPushButton("Quit", this);
-    quitButton->setGeometry(400, 400, 200, 50);
-
-    xDataLabel = new QLabel(this);
-    xDataLabel->setGeometry(100, 200, 100, 50);
-
-    yDataLabel = new QLabel(this);
-    yDataLabel->setGeometry(100, 250, 100, 50);
-
-    ThrottleSlider = new QSlider(this);
-    ThrottleSlider->setGeometry(300,400,100,50);
-
-
     // dem Worker-Objekt den Zeiger auf das gemeinsame Shared_Data_Container-Objekt geben:
     worker.sdc_ptr = &theSharedDataContainer;
 
@@ -74,17 +54,17 @@ MainWindow::MainWindow(QWidget *parent)
 
 
     // Wenn Quit-Button geklickt, zuerst den zusätzlichen Thread beenden:
-    connect(quitButton, SIGNAL(clicked()), &thread_for_worker, SLOT(quit()));
+    connect(ui->quitbutton, SIGNAL(clicked()), &thread_for_worker, SLOT(quit()));
 
     // Wenn zusätzlicher Thread beendet wurde, dann die gesamte App beenden:
     connect(&thread_for_worker, SIGNAL(finished()), qApp, SLOT(quit()));
 
     // Starten des Workers (zugehöriger Thread läuft bereits)
-    connect(startWorkerButton, SIGNAL(clicked()), &worker, SLOT(startWorking()));
+    connect(ui->startworker, SIGNAL(clicked()), &worker, SLOT(startWorking()));
     // Stoppen des Workers (Thread läuft aber weiter)
-    connect(stopWorkerButton, SIGNAL(clicked()), &worker, SLOT(stopWorking()));
+    connect(ui->closeworker, SIGNAL(clicked()), &worker, SLOT(stopWorking()));
 
-    connect(ui->pushButton_getdata, SIGNAL(clicked()), this, SLOT(getDataFromSharedDataObject()));
+    connect(ui->getdata, SIGNAL(clicked()), this, SLOT(getDataFromSharedDataObject()));
 
 }
 
@@ -98,22 +78,9 @@ void MainWindow::getDataFromSharedDataObject()
     // Lese Daten aus gemeinsamen Shared_Data_Container
     Data d = theSharedDataContainer.readData();
     //qDebug() << "Gelesen: x = " << d.x << " , y = " << d.y;
-    xDataLabel->setNum(d.x);
-    yDataLabel->setNum(d.y);
+    ui->label1->setNum(d.x);
+    ui->label2->setNum(d.y);
 }
-
-
-void MainWindow::on_pushButton_Add_clicked()
-{
-
-}
-
-void MainWindow::on_pushButton_Clear_clicked()
-{
-
-}
-
-
 
 void MainWindow::setupDemo(int demoIndex)
 {
